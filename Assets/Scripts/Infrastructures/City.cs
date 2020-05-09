b35440d49ada9e3d79b696889f21f5a8ec03cd28
Nicolas Schneider <nicolas06062000@hotmail.com>
2020-05-09 15:44:01 +0200

    Add City and TMP

---
+++ Assets/Scripts/Infrastructures/City.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class City : MonoBehaviour {
+    [Header("City Infos")] 
+    [SerializeField] SO_City soCity_;
+
+    [Header("UI")] 
+    [SerializeField] TextMeshProUGUI uiCityName_;
+    // Start is called before the first frame update
+    void Start() {
+        uiCityName_.text = soCity_.cityName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Infrastructures/SO_City.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "City")]
+public class SO_City : ScriptableObject {
+    public string cityName;
+    public int population;
+}
